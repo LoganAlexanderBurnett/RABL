168d5baedf33c853df98c826b17b59e7e449ae54
Logan A. Burnett <loganb14@mit.edu>
2026-01-20 11:32:03 -0500

    Add src layout and package metadata

---
+++ src/microreactor_dynamics/interface/__init__.py
@@ -0,0 +1,5 @@
+"""Interface utilities."""
+
+from microreactor_dynamics.interface.pymola import BatchConfig, DymolaBatchRunner
+
+__all__ = ["BatchConfig", "DymolaBatchRunner"]
---
+++ src/microreactor_dynamics/variography/__init__.py
@@ -0,0 +1,5 @@
+"""Variography tools and generators."""
+
+from microreactor_dynamics.variography.DrumVariography import DrumProfile, DrumProfileGenerator
+
+__all__ = ["DrumProfile", "DrumProfileGenerator"]
