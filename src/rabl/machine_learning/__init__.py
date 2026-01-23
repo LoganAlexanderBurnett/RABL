8ad91e3d72177e77fbaaadc8eb6873ef8594d4c0
Logan A. Burnett <loganb14@mit.edu>
2026-01-23 13:13:26 -0500

    Export LSTM dataloader helpers

--- src/rabl/machine_learning/__init__.py
+++ src/rabl/machine_learning/__init__.py
@@ -1,7 +1,15 @@
 """Machine learning utilities for RABL."""
 
 from .dataset_scaling import LSTMDatasetScalerSplitter
+from .inspect_lstm_dataloaders import build_datasets, build_model, rolling_forecast
 from .plot_feature_correlations import plot_feature_correlations
 from .plot_scaled_dataset import plot_scaled_features
 
-__all__ = ["LSTMDatasetScalerSplitter", "plot_scaled_features", "plot_feature_correlations"]
+__all__ = [
+    "LSTMDatasetScalerSplitter",
+    "build_datasets",
+    "build_model",
+    "rolling_forecast",
+    "plot_scaled_features",
+    "plot_feature_correlations",
+]
