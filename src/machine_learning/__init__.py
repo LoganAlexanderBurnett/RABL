19a4eddb029c7c179cf2e55bdc7595404eb56bd5
Logan A. Burnett <loganb14@mit.edu>
2026-01-23 10:04:56 -0500

    Add correlation plotter for scaled datasets

--- src/machine_learning/__init__.py
+++ src/machine_learning/__init__.py
@@ -1,6 +1,7 @@
 """Machine learning utilities for microreactor dynamics."""
 
 from .dataset_scaling import LSTMDatasetScalerSplitter
+from .plot_feature_correlations import plot_feature_correlations
 from .plot_scaled_dataset import plot_scaled_features
 
-__all__ = ["LSTMDatasetScalerSplitter", "plot_scaled_features"]
+__all__ = ["LSTMDatasetScalerSplitter", "plot_scaled_features", "plot_feature_correlations"]
