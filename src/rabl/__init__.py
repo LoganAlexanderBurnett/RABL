e3b0f4a085c2263f4a4c328bb6efe0009215fa61
LoganAlexanderBurnett <loganb14@mit.edu>
2026-01-26 15:02:09 -0700

    add toy dataset builder script, move build_lstm_dataset.py to src/rabl/machine_learning, update init.py's and make Dymola imports optional to avoid ModuleNotFound on HPC

--- src/rabl/__init__.py
+++ src/rabl/__init__.py
@@ -1,6 +1,12 @@
 """RABL (Microreactor Dynamics) Python package."""
 
-from rabl.interface.pymola import BatchConfig, DymolaBatchRunner
+try:
+    from rabl.interface.pymola import BatchConfig, DymolaBatchRunner
+except ModuleNotFoundError:
+    # Dymola is an optional dependency (not needed for ML on HPC)
+    BatchConfig = None
+    DymolaBatchRunner = None
+
 from rabl.variography.DrumVariography import DrumProfileGenerator, DrumProfile
 
 __all__ = [
